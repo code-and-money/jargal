cd72ee5415e43d5aebdc86d378da8675d932fd77
Alexey Sokolov <alexey@brand-map.online>
2025-10-09 16:12:49 +0300

    combine action

---
+++ actions/combine.ts
@@ -0,0 +1,10 @@
+import { executeAction } from "../runner.ts"
+import type { Action } from "../types.ts"
+
+export function combine( ...actions: Action[] ): Action {
+  return async function execute( params ) {
+    for ( const action of actions ) {
+      await executeAction( { action, context: params.context, renderer: params.renderer } )
+    }
+  }
+}
